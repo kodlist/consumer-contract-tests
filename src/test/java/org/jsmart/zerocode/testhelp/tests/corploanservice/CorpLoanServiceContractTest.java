7c31b78a4836d61aaea164243ed1c6348961a2b5
Author jSmart <author.japps@gmail.com>
2018-06-04 23:15:49 +0100

    ISS-86 # Contract test without hard coded values

--- src/test/java/org/jsmart/zerocode/testhelp/tests/corploanservice/CorpLoanServiceContractTest.java
+++ src/test/java/org/jsmart/zerocode/testhelp/tests/corploanservice/CorpLoanServiceContractTest.java
@@ -2,18 +2,21 @@ package org.jsmart.zerocode.testhelp.tests.corploanservice;
 
 import org.jsmart.zerocode.core.domain.JsonTestCase;
 import org.jsmart.zerocode.core.domain.TargetEnv;
+import org.jsmart.zerocode.core.domain.UseHttpClient;
+import org.jsmart.zerocode.core.httpclient.ssl.SslTrustHttpClient;
 import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @TargetEnv("corploan_server_host.properties")
+@UseHttpClient(SslTrustHttpClient.class)
 @RunWith(ZeroCodeUnitRunner.class)
 public class CorpLoanServiceContractTest {
 
 
     @Test
     @JsonTestCase("contract_tests/corploanservice/create_post_and_get_new_loan.json")
-    public void testNewLoan() throws Exception {
+    public void testNewLoan_crudOperations() throws Exception {
 
     }
 
